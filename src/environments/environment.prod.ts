c742d1edfbeb72573b63b9e489513c63389c85e2
DrZuzzjen <gutijeanf@gmail.com>
2021-11-23 17:25:10 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJVjZOgalydB-SPlLV2JRvIL2FlvdZySo",
+    authDomain: "mytechplan-c09b4.firebaseapp.com",
+    projectId: "mytechplan-c09b4",
+    storageBucket: "mytechplan-c09b4.appspot.com",
+    messagingSenderId: "234992302888",
+    appId: "1:234992302888:web:a94ce97e90657bdc6fee1d",
+    measurementId: "G-CP1MZP14XM"
+  }
 };
